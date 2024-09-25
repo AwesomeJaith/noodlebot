c6a8eaeb7338c65eeafeae1c8e6e521e5d0a9c6b
Jaith Darrah <generic.jaithd@gmail.com>
2024-09-25 02:07:58 -0500

    Update index.js to send image files over http

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,6 +2,8 @@ const { generateDiffHTML } = require("../utility/generateDiffHTML.js");
 const { captureHTMLFile } = require("../utility/html2png.js");
 
 const express = require("express");
+const fs = require("fs").promises;
+const os = require("os");
 
 const app = express();
 
@@ -9,14 +11,34 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/difference/:before/:after", (req, res) => {
-  const beforeText = req.params.before;
-  const afterText = req.params.after;
-  const html = generateDiffHTML(beforeText, afterText);
+app.get("/difference/:before/:after", async (req, res) => {
+  try {
+    const beforeText = req.params.before;
+    const afterText = req.params.after;
+    const html = generateDiffHTML(beforeText, afterText);
 
-  captureHTMLFile(html, "div.container", "output.png");
+    function generateUniqueFileName(extension = ".txt") {
+      const timestamp = Date.now(); // Current timestamp in milliseconds
+      const randomStr = Math.random().toString(36).substring(2, 15); // Random string
+      return `file_${timestamp}_${randomStr}${extension}`; // Example: file_1632864349625_abcd1234.txt
+    }
 
-  res.send(`Saved image!`);
+    const fileName = generateUniqueFileName(".png");
+
+    await captureHTMLFile(html, "div.container", `${fileName}`);
+
+    console.log(`${fileName}`);
+
+    const imagePath = `${fileName}`;
+
+    const data = await fs.readFile(imagePath);
+
+    res.setHeader("Content-Type", "image/png");
+    res.send(data);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Error handling the image file");
+  }
 });
 
 const server = app.listen(3000, function () {
